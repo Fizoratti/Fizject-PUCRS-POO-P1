4cbeecb74bfe126634127b5c188f17370a11f9cd
ROBSON FELIPE BITTENCOURT <15111092@pucrs.br>
2018-10-03 18:46:26 -0300

    Implement Geo class

---
+++ src/Geo.java
@@ -0,0 +1,9 @@
+public class Geo {
+    private double latitude;
+    private double longitude;
+
+    public Geo(double lat, double lon) {
+        this.latitude = lat;
+        this.longitude = lon;
+    }
+}
