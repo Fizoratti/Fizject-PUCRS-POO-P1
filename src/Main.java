2ebe72824ef3b7b3cb1043ce62109217d100b0e3
Robson <robson.felipe@acad.pucrs.br>
2018-10-04 16:40:28 -0300

    Implement questao 4 da prova

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void Main(String args[]) {
-        Imovel i1 = new Residencial();
+        //Questao 4:
+        //A)
+        CadastroImoveis<Imovel> ci = new CadastroImoveis<>();
+
+        //B)
+        ci.add(new Residencial("Robson", 70, 12.456, 54.321));
+        ci.add(new Residencial("Jose", 120, 13.456, 11.721));
+        ci.add(new Residencial("Bianca", 400, 12.878, 54.321));
+        
+        //C)
+        System.out.println(ci.listar());
+
+        //D)
+        System.out.println(ci.listar("John Doe"));
+
+        //E)
+        System.out.println(ci.calculaImpostoTotal());
+
     }
 }
