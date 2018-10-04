0b18e74c914d566bda78e2da05244f2768c75096
Robson <robson.felipe@acad.pucrs.br>
2018-10-04 13:36:26 -0300

    Add getter/setter and toString to Geo class

--- src/Geo.java
+++ src/Geo.java
@@ -6,4 +6,22 @@ public class Geo {
         this.latitude = lat;
         this.longitude = lon;
     }
+
+    public double getLatitude() {
+        return this.latitude;
+    }
+    public void setLatitude(double lat) {
+        this.latitude = lat;
+    }
+    public double getLongitude() {
+        return this.longitude;
+    }
+    public void setLatitude(double lon) {
+        this.longitude = lon;
+    }
+
+    @Override
+    public String toString() {
+        return "(lat) "+getLatitude()+" (lon) "+getLongitude();
+    }
 }
